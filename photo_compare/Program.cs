6c536ce7079e453c3024d329d9b57038f830050f
Javier Romero <ajangles@gmail.com>
2021-01-28 12:52:35 +1100

    Add project files.

---
+++ photo_compare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace photo_compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ photo_compare_tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace photo_compare_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
